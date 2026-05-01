cd460f38e097e11857195fba8f36772da5e706de
EdvinArnryd <edvin.arn@hotmail.com>
2026-05-01 15:15:15 +0200

    Added jump

--- ZEngine/Assets/Scripts/PlayerController.cs
+++ ZEngine/Assets/Scripts/PlayerController.cs
@@ -3,33 +3,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private InputSystem_Actions inputActions;
+    private InputSystem_Actions _inputActions;
+
+    private Vector2 _moveInput;
+    private Rigidbody rb;
 
     void Awake()
     {
-        inputActions = new InputSystem_Actions();
+        _inputActions = new InputSystem_Actions();
+        rb = GetComponent<Rigidbody>();
 
-        inputActions.Player.Move.performed += OnMove;
-        inputActions.Player.Jump.performed += OnJump;
+        _inputActions.Player.Move.performed += OnMove;
+        _inputActions.Player.Move.canceled += OnMove;
+        _inputActions.Player.Jump.performed += OnJump;
+        _inputActions.Player.Jump.canceled += OnJump;
     }
 
     private void OnEnable()
     {
-        inputActions.Player.Enable();        
+        _inputActions.Player.Enable();        
     }
 
     private void OnDisable()
     {
-        inputActions.Player.Disable();       
+        _inputActions.Player.Disable();       
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 movement = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        transform.Translate(movement * 5 * Time.deltaTime);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log("Player Moved!");
+        _moveInput = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log("Player Jumped!");
+        rb.AddForce(0,4,0, ForceMode.Impulse);
     }
 }
